b82c33f98c003fc776061ddb5d0ffb22626b29b3
Whitby <whitbys@vdi-sccubu-014.lancs.local>
2022-03-13 01:20:02 +0000

    blank board created

--- GUI.java
+++ GUI.java
@@ -1,13 +1,26 @@
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
-
+//result mini grids
+//background colour so gaps aren't visible
 public class GUI{
-    JPanel p = new JPanel();
+    private int comboNum = 4;
+    private int guessNum = 6;
+    private int boardNum = comboNum * guessNum;
+    
+    
+    JPanel pMain = new JPanel(new BorderLayout());
+    JPanel pBut = new JPanel(new GridLayout());
+    JPanel pBoard = new JPanel(new GridLayout(guessNum, comboNum));
+    JPanel pResult = new JPanel(new GridLayout(guessNum, 1));
+
+
+
     JFrame f = new JFrame("CODE BREAKER");
 
     GridLayout gl = new GridLayout();
@@ -24,23 +37,50 @@ public class GUI{
     Picture correct1 = new Picture("Score_1.png");
 
     JButton redBut = new JButton(red);
+    JButton orangeBut = new JButton(orange);
+    JButton yellowBut = new JButton(yellow);
+    JButton greenBut = new JButton(green);
+    JButton blueBut = new JButton(blue);
+    JButton indigoBut = new JButton(indigo);
+    JButton violetBut = new JButton(violet);
 
+    JLabel[] boardLabel = new JLabel[boardNum];
+    JLabel[] resultLabel = new JLabel[boardNum];
 
+    JPanel[] pRowResult = new JPanel[guessNum];
     
-
-
-
-
     public GUI(){
+        f.setContentPane(pMain);
+        
+        pMain.add(pBut, BorderLayout.SOUTH);
+        pMain.add(pBoard, BorderLayout.CENTER);
+        pMain.add(pResult, BorderLayout.EAST);
 
-        f.setContentPane(p);
-        p.setLayout(new GridLayout());
+        for(int i = 0; i < guessNum; i++){
+            pRowResult[i] = new JPanel(new GridLayout(2,2));
+            pResult.add(pRowResult[i]);
 
-        p.add(redBut);
+            for(int j = 0; j < comboNum; j++){
+                resultLabel[j] = new JLabel(empty);
+                pRowResult[i].add(resultLabel[j]);
+            }
+        }
         
         
         
+ 
+        pBut.add(redBut);
+        pBut.add(orangeBut);
+        pBut.add(yellowBut);
+        pBut.add(greenBut);
+        pBut.add(blueBut);
+        pBut.add(indigoBut);
+        pBut.add(violetBut);
         
+        for(int k = 0; k < boardNum; k++){
+            boardLabel[k] = new JLabel(empty);
+            pBoard.add(boardLabel[k]);
+        }
         
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(900, 900);
